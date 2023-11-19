dc3cba8891a283714f5f132fef46d2601a1c4f71
R Li <lry.ox.uk@gmail.com>
2023-11-19 19:27:43 +0000

    ex 3.13

--- index.js
+++ index.js
@@ -27,12 +27,9 @@ app.get('/info',(request,response)=>{
 })
 
 app.get('/api/persons/:id',(request,response)=>{
-    const id = Number(request.params.id)
-    const person = persons.find(person=>person.id === id)
-    if(person){response.json(person)}
-    else{
-      response.status(404).end()
-    }
+  Person.findById(request.params.id).then(person => {
+    response.json(person)
+  })
 })
 
 app.delete('/api/persons/:id',(request,response)=>{
