d900515a4e2a9747543bdf1f63db72db1ddaa63a
R Li <lry.ox.uk@gmail.com>
2023-11-19 20:24:45 +0000

    ex 3.14

--- index.js
+++ index.js
@@ -20,27 +20,27 @@ app.get('/api/persons', (request, response) => {
     Person.find().then(persons => {response.json(persons)})
   })
 
+/*
 app.get('/info',(request,response)=>{
     response.send(`<p>Phonebook has info for ${persons.length} people</p>
                     <br/>
                     <p>${Date()}</p>`)
 })
-
+*/
 app.get('/api/persons/:id',(request,response)=>{
   Person.findById(request.params.id).then(person => {
     response.json(person)
   })
 })
 
+/*
 app.delete('/api/persons/:id',(request,response)=>{
     const id = Number(request.params.id)
     persons = persons.filter(person => person.id !== id)
     response.status(204).end()
 })
+*/
 
-const generateId =()=>{
-    return Math.floor(Math.random() * 100000)
-}
 
 app.post('/api/persons',(request,response)=>{
     const body = request.body
@@ -49,7 +49,7 @@ app.post('/api/persons',(request,response)=>{
         error: 'entry must include a name and a number' })
     }
     /*
-    else if(persons.find(person =>person.name === body.name)){
+    else if(Person.find({name:body.name})){
         return response.status(400).json({ 
             error: 'name must be unique' })
     }
@@ -62,6 +62,7 @@ app.post('/api/persons',(request,response)=>{
     person.save().then(savedPerson =>{response.json(savedPerson)})
     
 })
+
 
 
 
